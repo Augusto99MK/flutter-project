c006d978d485d9f906daebddf26bee7a705cfb36
Augusto99MK <carlosaugustox6@gmail.com>
2023-12-07 15:42:56 -0400

    Crud Completo

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,29 @@
+import '../models/user.dart';
+
+// ignore: non_constant_identifier_names
+Map<String, User> DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: "Daniel",
+      email: "daniel@email.com",
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/06/15/16/16/man-1459246_1280.png'),
+  '2': const User(
+      id: '2',
+      name: "Joffrey",
+      email: "joffrey@kingsland.com",
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2019/02/13/04/41/girl-3993644_1280.png'),
+  '3': const User(
+      id: '3',
+      name: "Gandalf",
+      email: "gandalf@middlearth.com",
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/13/43/gandalf-161456_1280.png'),
+  '4': const User(
+      id: '2',
+      name: "Wesker",
+      email: "wesker@umbrella.com",
+      avatarUrl:
+          'https://media.istockphoto.com/id/1420764187/pt/foto/child-with-umbrella-wonders-rainy-weather-walking-outdoor-toddler-girl-wearing-red-raincoat.jpg?s=612x612&w=is&k=20&c=xVTakJIJt4FZQjL4pC8It1aKhceNHJghM3Is6Ph-tiA='),
+};
